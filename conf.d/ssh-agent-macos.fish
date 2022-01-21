0b4b863cff5aa1de7ff99f37d5d9fd6e21ceb52c
Daniel Kempkens <daniel+git@kempkens.io>
2022-01-22 00:15:30 +0100

    Switch to ps in conf.d script, too

--- conf.d/ssh-agent-macos.fish
+++ conf.d/ssh-agent-macos.fish
@@ -8,7 +8,7 @@ if not test -e "$SSH_ENV"
 else if test -e "$SSH_ENV"; and test -z "$SSH_AGENT_PID"
     # Environment file exists but $SSH_AGENT_PID is not set
     __ssh_agent_source
-else if not pgrep "$SSH_AGENT_PID"
+else if not ps -p "$SSH_AGENT_PID" >/dev/null 2>&1
     # The process that $SSH_AGENT_PID references is not alive
     __ssh_agent_start
 end
