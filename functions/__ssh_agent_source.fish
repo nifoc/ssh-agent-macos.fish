77176f7577ccc4da4bc811f0039a05c9a2a72e01
Daniel Kempkens <daniel+git@kempkens.io>
2022-01-22 00:12:52 +0100

    Switch from pgrep to ps for PID checking

--- functions/__ssh_agent_source.fish
+++ functions/__ssh_agent_source.fish
@@ -2,7 +2,7 @@ function __ssh_agent_source -d "source ssh-agent environment"
     # Source existing $SSH_ENV
     source "$SSH_ENV" >/dev/null
 
-    if test -z "$SSH_AGENT_PID"; or not pgrep "$SSH_AGENT_PID" >/dev/null
+    if test -z "$SSH_AGENT_PID"; or not ps -p "$SSH_AGENT_PID" >/dev/null 2>&1
         # Something is wrong with $SSH_ENV, let's restart ssh-agent
         __ssh_agent_start
     end
